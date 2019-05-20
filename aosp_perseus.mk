f52bb78c4790751db7d588ec20ca59ebd7015079
JoseVega <jdelavega36@gmail.com>
2019-05-20 10:48:21 -0500

    Perseus:Initial CAF bringup

--- aosp_perseus.mk
+++ aosp_perseus.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/perseus/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
